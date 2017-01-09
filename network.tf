195d9a7b3a0149dbd29afa9d586bebdbcb785f8c
Jean-Marie Ferdegue <jeanmarie.ferdegue@opencredo.com>
2017-01-09 12:50:20 +0000

    Create subnets

--- network.tf
+++ network.tf
@@ -6,3 +6,21 @@ resource "aws_vpc" "main" {
         Author = "Jean-Marie"
     }
 }
+
+variable "availability_zones" {
+  type    = "map"
+  default = {
+    eu-west-1 = "eu-west-1a,eu-west-1b,eu-west-1c"
+  }
+}
+
+resource "aws_subnet" "fromMainVpc" {
+  vpc_id            = "${aws_vpc.main.id}"
+  count             = 3
+  availability_zone = "${element(split(",", lookup(var.availability_zones, var.region)), count.index)}"
+  cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
+  tags {
+      Name = "Subnet Jean-Marie"
+      Author = "Jean-Marie"
+  }
+}
