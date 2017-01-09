9e71c6b0ef9ca2b638ed260a522826225e14d037
Jean-Marie Ferdegue <jeanmarie.ferdegue@opencredo.com>
2017-01-09 12:50:55 +0000

    Create 3 instances, one in each subnet

---
+++ etcd.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "etcd" {
+    ami = "${data.aws_ami.ubuntu.id}"
+    count = 3
+    subnet_id = "${element(aws_subnet.fromMainVpc.*.id, count.index)}"
+    instance_type = "t2.micro"
+    tags {
+        Name = "Etcd Jean-Marie"
+        Author = "Jean-Marie"
+    }
+}
