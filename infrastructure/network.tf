dec99ec9e719d58da9bf9a9c15a2686f7c1315c9
Jean-Marie Ferdegue <jeanmarie.ferdegue@opencredo.com>
2017-01-10 11:36:27 +0000

    Allow outbound connection to web resources

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -73,6 +73,20 @@ resource "aws_security_group" "etcd" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    protocol = "TCP"
+    from_port = 80
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol = "TCP"
+    from_port = 443
+    to_port = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags {
     Name = "Jean-Marie"
     Owner = "${var.ownerName}"
